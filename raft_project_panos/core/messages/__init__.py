c4f354e07bd502125868b8610ee1f1795101a588
Costas <210527419+costas-g@users.noreply.github.com>
2025-06-24 17:57:19 +0300

    isolate branches

--- raft_project_panos/core/messages/__init__.py
+++
@@ -1,3 +0,0 @@
-from .request_vote import RequestVote, RequestVoteResponse
-from .append_entries import AppendEntries, AppendEntriesResponse
-from .base import RaftMessage
--- raft_project_panos/core/messages/base.py
+++
@@ -1,4 +0,0 @@
-class RaftMessage:
-    def __init__(self, term, sender_id):
-        self.term = term
-        self.sender_id = sender_id
